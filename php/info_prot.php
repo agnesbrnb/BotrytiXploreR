25cb77b07806a06612932fdd6501c86e77a116d9
Agnes Barnabe <gnessou91@gmail.com>
2018-11-19 16:08:09 +0100

    Edit bubble position

--- php/info_prot.php
+++ php/info_prot.php
@@ -28,7 +28,7 @@ session_start();
           Chercher un autre gène : BC1G_<input type="text" name="id"
             value=<?php
               if($_SESSION['var']!=""){echo $_SESSION['var'];}else{echo "00001";}
-            ?> maxlength="5">
+            ?> maxlength="5" size="15">
           <input type="submit" value="Go !">
 
           <a href="info_gene.php">Le gène<img class="bulle gene" src="../img/bulle_gene.png" alt="Gène" /></a>
