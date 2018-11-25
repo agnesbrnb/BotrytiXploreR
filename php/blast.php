04d5da5ecf8f6ae6ad3743e20fcc71cefa090cfb
MartinDavy <martin.davy@u-psud.fr>
2018-11-25 22:22:29 +0100

    Ajout du blast, plus cas l'afficher

--- php/blast.php
+++ php/blast.php
@@ -20,6 +20,87 @@ session_start();
       include "./bandeau_blast.php";
      ?>
 
+     <!-- Realisation et lecture du blast -->
+
+    <?php
+      if (isset($_POST['seq']))
+      {
+        // initialisation de la sequence
+        $seq = "";
+        // Explosion de seq en fonction des \n
+        $fasta = explode("\n", $_POST['seq']);
+        // creation / "ecrasement" de query.fasta
+        $query_file = fopen('query.fasta', 'w');
+        
+        // Boucle sur les "morceaux" de l'explosion
+        for ($i=0; $i < sizeof($fasta); $i++) 
+        {
+          // traitement de la premiere ligne
+          if ($i == 0) 
+          {
+            // si elle commence avec > alors il y a un ligne de description
+            if(substr($fasta[$i], 0, 1) != '>')
+            {
+              // sinon ecriture d'un ligne generique pour respecter le format fasta
+              fputs($query_file, "> query\n");
+              // concatenation de la sequence
+              $seq .= $fasta[$i];
+            }
+            else
+            {
+              // Ecriture de la ligne descriptive - (celle avec ">")
+              fputs($query_file, $fasta[$i]);
+            }
+          }
+          else
+          {
+            // concatenation de la sequence
+            $seq .= $fasta[$i];
+          }
+
+        }
+        // Suppression des espaces present dans la sequence
+        $seq = preg_replace("/\s+/", "", $seq);
+        // Toutes les lettres sont mises en majuscule
+        $seq = strtoupper($seq);
+        // ecriture de la sequence
+        fputs($query_file, $seq);
+        // fermeture
+        fclose($query_file);
+
+        // Realisation du blast
+        $user = get_current_user();
+        if ($user == "martin") {       
+          exec ("blastp -query query.fasta -db ../bd/prot_db/botrytis_prot_db -out result.blastp");
+
+        }elseif ($user == "agnesb") {
+          exec ("/usr/local/bin/blastp -query query.fasta -db ../bd/prot_db/botrytis_prot_db -out result.blastp");
+        }
+
+        // Affichage du resultat blast
+        // ouverture du blastp
+        $blastp_file = fopen('result.blastp', 'r');
+        $ligne = "";
+        $result = array();
+        // Lecture jusqu'aux resultats
+        while (substr($ligne, 0, 9) != "Sequences") 
+        {
+          $ligne = fgets($blastp_file);
+        }
+        // Saute la ligne vide
+        $ligne = fgets($blastp_file);
+        $ligne = fgets($blastp_file);
+        $hits  = array();
+        while ($ligne != "\n") 
+        {
+          $ligne = fgets($blastp_file);   
+          echo $ligne;     
+          $hits["gene_id"] = substr($ligne, 14, 25);
+        }
+
+      }
+    ?>
+
     <div id="menu">
 
       <hr>
@@ -28,11 +109,12 @@ session_start();
       <p>Cette recherche de séquence s'effectue sur la base des gènes de
         <i>Botrytis cinerea</i>. Entrez votre séquence ci-dessous :</p>
 
-      <textarea id="seq" name="seq" rows="20" cols="100"
-      style="margin-left:10px">Votre séquence ...
-      </textarea>
+    <form action="blast.php" method="post">      
+      <textarea id="seq" name="seq" rows="20" cols="100" 
+      style="margin-left:10px" placeholder=" Votre séquence ..."></textarea>
 
-      <input type="submit" value="Lancer le Blast">
+      <input type="submit" value="Blaster !">
+    </form>
 
     </div>
 
